599074b7536992b879ea93b4191ed21581fba2d7
江口純矢 <s_eguchi@codmon.co.jp>
2024-07-07 22:50:59 +0900

    isEnglishを追加

--- Capters/Capter9/src/SelfMade/Book.php
+++ Capters/Capter9/src/SelfMade/Book.php
@@ -4,15 +4,22 @@ namespace Capters\Capter9\src\SelfMade;
 
 class Book
 {
-  private string $name;
+  private readonly string $name;
+  private readonly bool $isEnglish;
 
-  public function __construct(string $name)
+  public function __construct(string $name, bool $isEnglish = true)
   {
     $this->name = $name;
+    $this->isEnglish = $isEnglish;
   }
 
   public function getName(): string
   {
     return $this->name;
+  }
+
+  public function isEnglish(): string
+  {
+    return $this->isEnglish;
   }
 }
