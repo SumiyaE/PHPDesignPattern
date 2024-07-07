0ad3fd5cc133c68ae95384fff3b462e6f74b40fe
江口純矢 <s_eguchi@codmon.co.jp>
2024-07-07 22:41:45 +0900

    逆順の場合の処理を追加

--- Capters/Capter9/src/BookShelfIterator.php
+++ Capters/Capter9/src/BookShelfIterator.php
@@ -6,24 +6,27 @@ use Capters\Capter9\src\Interfaces\IteratorInterface;
 
 class BookShelfIterator implements IteratorInterface
 {
-  private $bookShelf;
-  private $index;
+  private BookShelf $bookShelf;
+  private int $index;
 
   public function __construct(BookShelf $bookShelf)
   {
     $this->bookShelf = $bookShelf;
     $this->index = 0;
+    // $this->index = this->bookShelf->getLength() - 1;
   }
 
   public function hasNext(): bool
   {
     return $this->index < $this->bookShelf->getLength();
+    // return $this->index >= 0;
   }
 
   public function next(): object
   {
     $book = $this->bookShelf->getBookAt($this->index);
     $this->index++;
+    // $this->index--;
 
     return $book;
   }
--- Capters/Capter9/src/Client.php
+++ Capters/Capter9/src/Client.php
@@ -11,15 +11,19 @@ function main(): void
   $bookShelf->appendBook(new Book('book-C'));
   $bookShelf->appendBook(new Book('book-D'));
 
+  // for文へのアクセス方法をクライアントが知っている
+  // booksの内部構造が変わる場合、たとえば連想配列に変更した場合、クライアントも更する必要がある
+  // booksの列挙方法が変わる場合、たとえば逆順で取得したい場合、クライアントも変更する必要がある
+  $books = $bookShelf->getBooks();
+  for ($i = 0; $i < count($books); $i++) {
+    echo $books[$i]->getName() . "\n";
+  }
+
   $iterator = $bookShelf->iterator();
   while ($iterator->hasNext()) {
     echo $iterator->next()->getName() . "\n";
   }
 
-  $books = $bookShelf->getBooks();
-  for ($i = 0; $i < count($books); $i++) {
-    echo $books[$i]->getName() . "\n";
-  }
 }
 
 main();
